32ba391e58b1690c69333e02ffa175fc24b36d58
Julian de Ruiter <julianderuiter@gmail.com>
2017-09-12 10:46:02 +0200

    Fix import.

--- src/genopandas/__init__.py
+++ src/genopandas/__init__.py
@@ -1,6 +1,5 @@
 from .model import (GenomicDataFrame, GenomicIndex, RegionMatrix, FeatureMatrix,
                     Interval, IntervalTree, GenomicIntervalTree)
-from .tree import Interval, IntervalTree, GenomicIntervalTree
 
 __author__ = 'Julian de Ruiter'
 __email__ = 'julianderuiter@gmail.com'
